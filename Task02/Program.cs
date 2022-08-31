d33e9e97ac248cbb43ffa42881a4455d78e03a93
NKompa <nkompa7@gmail.com>
2022-09-01 01:43:14 +0300

    Task 2 Sum of digits

---
+++ Task02/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе: 452 -> 11, 82 -> 10.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+while(number > 0)
+{
+    result = result + number%10;
+    number = number/10;
+}
+Console.WriteLine($"Результат: {result}");
